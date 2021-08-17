9dc8fb6899d80a462f91897d2ec3c585fffc7665
GaiaRossi <gaia.rossi@student.unife.it>
2021-08-17 15:20:41 +0200

    modificato il codice per il controllo del braccio

--- python_node-red_move/main.py
+++ python_node-red_move/main.py
@@ -1,24 +1,58 @@
+from gpiozero.pins.pigpio import PiGPIOFactory
 import paho.mqtt.client as mqtt
 import json
 import gpiozero
 import time
 
+class Arm():
+	def __init__(self, base, gripper, elbow, shoulder):
+		self.base = base
+		self.gripper = gripper
+		self.elbow = elbow
+		self.shoulder = shoulder
+
+		self.parts = {
+			"base": self.base,
+			"gripper": self.gripper,
+			"elbow": self.elbow,
+			"shoulder": self.shoulder
+		}
+
+		self.MIN_VALUE = -1
+		self.MAX_VALUE = 1
+		self.STEP = 0.01
+		self.sensitivity = 0.01
+
+		self.multiplier = 1
+		self.is_idle = True
+
+	def move(self, part_name, value):
+		if value <= self.MIN_VALUE or value >= self.MAX_VALUE:
+			return -1
+
+		self.is_idle = False
+		#se il valore in cui mi trovo adesso
+		#e piu grande di quello che devo assumere
+		#allora dovro diminuire, altrimenti aumentare
+		if self.parts[part_name].value > value:
+			self.multiplier = -1
+		else:
+			self.multiplier = 1
+		while not (self.parts[part_name].value > value - self.sensitivity and self.parts[part_name].value < value + self.sensitivity):
+			self.parts[part_name].value = self.parts[part_name].value + self.multiplier*self.STEP
+			print(self.parts[part_name].value)
+			#time.sleep(0.3)
+
+		self.is_idle = True
+		return 0
+
 #variabili globali
-base = gpiozero.Servo(4)
-gripper = gpiozero.Servo(10)
-elbow = gpiozero.Servo(17)
-shoulder = gpiozero.Servo(22)
+base = gpiozero.Servo(4, pin_factory = PiGPIOFactory())
+gripper = gpiozero.Servo(10, pin_factory = PiGPIOFactory())
+elbow = gpiozero.Servo(17, pin_factory = PiGPIOFactory())
+shoulder = gpiozero.Servo(22, pin_factory = PiGPIOFactory())
 
-min = -1
-max = 1
-
-#facilito la ricerca della parte del braccio da muovere
-parts = {
-	"base": base,
-	"gripper": gripper,
-	"elbow": elbow,
-	"shoulder": shoulder
-}
+arm = Arm(base, gripper, elbow, shoulder)
 
 def on_connect(client, userdata, flags, rc):
 	print("Connesso al broker con codice {}".format(str(rc)))
@@ -28,35 +62,22 @@ def on_connect(client, userdata, flags, rc):
 	print("Iscritto al topic meArm")
 
 def on_message(client, userdata, msg):
+
+	global arm
+
 	#print("Topic: {}, messaggio: {}".format(msg.topic, msg.payload.decode("UTF-8")))
 
 	#leggo messaggio con json per comodita
 	json_msg = json.loads(msg.payload.decode("UTF-8"))
-	print("Da muovere: {}".format(json_msg["part"]))
-	print("Valore angolo: {}".format(json_msg["value"]))
+	values = {
+		"base": json_msg["base"],
+		"gripper": json_msg["gripper"],
+		"elbow": json_msg["elbow"],
+		"shoulder": json_msg["shoulder"]
+	}
 
-	part = json_msg["part"]
-	value = json_msg["value"]
-
-	status = move_arm(part, value)
-	if status != 0:
-		client.publish("meArm_response", "movimento non effettuato")
-	else:
-		#pubblico risposta di avvenuto movimento
-		client.publish("meArm_response", "movimento effettuato")
-
-
-def move_arm(part, value):
-
-	global parts
-	global min
-	global max
-
-	if value > max or value < min:
-		return -1
-
-	parts[part].value = value
-	return 0
+	for part_name, part  in arm.parts.items():
+		status = arm.move(part_name, values[part_name])
 
 
 #main
@@ -69,4 +90,9 @@ port = 1883
 
 client.connect(server_ip_address, port)
 
-client.loop_forever()
+try:
+	client.loop_forever()
+except KeyboardInterrupt:
+	print("Disconnessione")
+	client.disconnect()
+	exit(0)
